6669e48dfea766e07f773f45ebe259762d96fc73
Lance Coombes - DSV <lance.coombes@us.dsv.com>
2016-12-01 16:25:06 -0800

    Testing database connectivity, adding and deleting rows.

---
+++ blogpostapi.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+# import sqlite3 module and connect to the database
+import sqlite3
+conn = sqlite3.connect('blog.db')
+
+#Create cursor
+c = conn.cursor()
+
+# Insert a row of data
+c.execute("INSERT INTO posts VALUES ('1','TEST','Testing')")
+
+# Commit changes
+conn.commit()
+
+# Select all rows and print to STDOUT
+for row in c.execute("SELECT * FROM posts"):
+  print row
+
+# Cleanup while testing.  Leave DB empty.
+c.execute("DELETE FROM posts")
+conn.commit()
+conn.close()
